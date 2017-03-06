537f3252d2dc2f992b571beef31e15c5adeba1da
ttraaholt <ttraaholt@css.edu>
2017-03-06 17:50:25 -0600

    App is working. Comments were also added.

--- app/src/main/java/com/example/ttraaholt/assignment7flooring/Main2Activity.java
+++ app/src/main/java/com/example/ttraaholt/assignment7flooring/Main2Activity.java
@@ -6,24 +6,28 @@ import android.os.Bundle;
 import android.widget.TextView;
 
 public class Main2Activity extends AppCompatActivity {
-
+    //Declare the TextView that will be changed
     TextView tvResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
-        tvResult = (TextView) findViewById(R.id.textViewResult);
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
-
+        //Call the Intent
         Intent Intent = getIntent();
+        //Place the Strings passed into two new variables l and w.
         String l = Intent.getStringExtra("length");
         String w = Intent.getStringExtra("width");
-        Double length = Double.parseDouble(w);
-        Double width = Double.parseDouble(l);
+        //Parse the strings to doubles
+        Double width = Double.parseDouble(w);
+        Double length = Double.parseDouble(l);
+        //create a result variable that takes the width and the length and multiplies them together.
         Double result = width * length;
 
-        tvResult.setText("Flooring needed: Width is " + width + " and length is " + length + " and flooring needed is " + result);
+        //Locat the text view from the xml file.
+        tvResult = (TextView) findViewById(R.id.textViewResult);
+        //set the text view to include the width, length, and result.
+        tvResult.setText("Flooring needed: Width is " + width + " and Length is " + length + " and flooring needed is " + result);
     }
 }
--- app/src/main/java/com/example/ttraaholt/assignment7flooring/MainActivity.java
+++ app/src/main/java/com/example/ttraaholt/assignment7flooring/MainActivity.java
@@ -8,26 +8,27 @@ import android.widget.EditText;
 
 
 public class MainActivity extends AppCompatActivity {
-
+    //Creates two EditText variables
     EditText etLength, etWidth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        //When the onCreate method is called, attribute etLength and etWidth with their respective Text Fields.
         etLength = (EditText) findViewById(R.id.editTextLength);
         etWidth = (EditText) findViewById(R.id.editTextWidth);
     }
-
+        //On click calculate passes the intent to the Main2Activity and grabs the strings in the two text fields.
         public void onClickCalculate(View v) {
 
         Intent Intent = new Intent(MainActivity.this, Main2Activity.class);
             String width = etWidth.getText().toString();
             String length = etLength.getText().toString();
+            //Attribute the width and length into an extra and send it with the key "length" and "width".
         Intent.putExtra("length",length);
         Intent.putExtra("width",width);
-
+        //Start the Intent
         startActivity(Intent);
 
     }
